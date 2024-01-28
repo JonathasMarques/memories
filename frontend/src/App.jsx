712d37546191d6ccb2080d587e1c7d4a8b25353b
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-01-28 17:10:28 -0300

    memories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 
+import { Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Memories</h1>
+      <div className='App'>
+        <Navbar />
+        <div className="container">
+          <Outlet/>
+        </div>
+        <Footer />
       </div>
     </>
   )
